e5e47b0fc7869228930f3581693c833f30925bdf
Andrei Dodu <andrei.dodu@hotmail.it>
2022-08-31 22:06:49 +0200

    feat: added configurable timeout setting

--- src/main/java/org/isf/generaldata/GeneralData.java
+++ src/main/java/org/isf/generaldata/GeneralData.java
@@ -89,6 +89,7 @@ public final class GeneralData extends ConfigurationProperties {
 	public static String PATIENTBILLGROUPED;
 	public static String PATIENTBILLSTATEMENT;
 	public static boolean DEBUG;
+	public static Integer SESSIONTIMEOUT;
 
 	public static boolean STRONGPASSWORD;
 	public static int STRONGLENGTH;
@@ -138,6 +139,7 @@ public final class GeneralData extends ConfigurationProperties {
 	private static final String DEFAULT_PATIENTBILLSTATEMENT = "PatientBillStatement";
 	private static final boolean DEFAULT_DEBUG = false;
 	private static final int DEFAULT_STRONGLENGTH = 10;
+	private static final int DEFAULT_SESSIONTIMEOUT=300000;
 
 	private static final boolean DEFAULT_STRONGPASSWORD = true;
 
@@ -204,6 +206,7 @@ public final class GeneralData extends ConfigurationProperties {
 		if (STRONGLENGTH < 6) {
 			STRONGLENGTH = 6;
 		}
+		SESSIONTIMEOUT = myGetProperty("SESSIONTIMEOUT", DEFAULT_SESSIONTIMEOUT);
 	}
 
 	public static GeneralData getGeneralData() {
